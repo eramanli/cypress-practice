870f220bcec34ce403f9d701981157c76d9af7fc
erdinc.ramanli <erdinc.ramanli@trendyol.com>
2022-04-28 01:33:43 +0300

    Add uncaught exception

--- cypress/support/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Cypress {
-    interface Chainable {
-        selectCountry(country: string): Chainable
-    }
-}
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,10 @@
 import './command.js'
 
 Cypress.config('defaultCommandTimeout', 10000)
+
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
